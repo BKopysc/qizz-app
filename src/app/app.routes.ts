99c22e20bfcf2c06a0908a8ed0adc1a9bbab35ef
Bartlomiej Kopysc <bkopysc@wp.pl>
2024-02-29 01:42:08 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GeneratorComponent } from './pages/generator/generator.component';
 
-export const routes: Routes = [];
+
+export type CustomRoute = 'home' | 'generator' | 'about';
+
+export const routes: Routes = 
+[{ path: '', redirectTo: 'home', pathMatch: 'full'},
+{path: 'home', component: HomeComponent},
+{path: 'generator', component: GeneratorComponent},
+];
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { CustomRoute } from '../../app.routes';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    CommonModule,
+    RouterModule
+  ]
+})
+
+export class NavbarComponent {
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  constructor(private router: Router) {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+    onRouteSelect(name: CustomRoute) {
+      this.router.navigate([name]);
+    }
+}
